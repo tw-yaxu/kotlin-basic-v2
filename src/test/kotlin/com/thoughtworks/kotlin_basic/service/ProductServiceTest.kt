0f1d9f6d6fc773eeb40f2db9769ec45b27ebacbd
ya xu <yaxu@thoughtworks.com>
2024-09-02 18:57:50 +0800

    add high demand product scenarios

--- src/test/kotlin/com/thoughtworks/kotlin_basic/service/ProductServiceTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/service/ProductServiceTest.kt
@@ -33,12 +33,51 @@ class ProductServiceTest {
         assertEquals(2, result.size)
     }
 
+    @Test
+    fun `should get high demand products with original price when stock is greater than 100`(): Unit = runBlocking {
+        val mockProducts = listOf(PRODUCT_2)
+        val mockInventories = listOf(INVENTORY_2, INVENTORY_3)
+
+        coEvery { apiService.getAllProducts().execute() } returns Response.success(mockProducts)
+        coEvery { apiService.getAllInventories().execute() } returns Response.success(mockInventories)
+
+        val result = productService.getAllProductWithInventory()
+        assertEquals(1, result.size)
+        assertEquals(20F, result[0].price)
+    }
+
+    @Test
+    fun `should get high demand products with 120 percents of the original price when stock is less than or equal to 100 and greater than 30`(): Unit = runBlocking {
+        val mockProducts = listOf(PRODUCT_2)
+        val mockInventories = listOf(INVENTORY_2)
+
+        coEvery { apiService.getAllProducts().execute() } returns Response.success(mockProducts)
+        coEvery { apiService.getAllInventories().execute() } returns Response.success(mockInventories)
+
+        val result = productService.getAllProductWithInventory()
+        assertEquals(1, result.size)
+        assertEquals(24F, result[0].price)
+    }
+
+    @Test
+    fun `should get high demand products with 150 percents of the original price when stock is less than or equal to 30`(): Unit = runBlocking {
+        val mockProducts = listOf(PRODUCT_2)
+        val mockInventories = listOf(INVENTORY_3)
+
+        coEvery { apiService.getAllProducts().execute() } returns Response.success(mockProducts)
+        coEvery { apiService.getAllInventories().execute() } returns Response.success(mockInventories)
+
+        val result = productService.getAllProductWithInventory()
+        assertEquals(1, result.size)
+        assertEquals(30F, result[0].price)
+    }
+
     companion object {
         private val PRODUCT_1 = Product(id = 1, name = "Product 1", SKU = "SKU1", price = 10F, image = "image1", type = ProductType.NORMAL)
         private val PRODUCT_2 = Product(id = 2, name = "Product 2", SKU = "SKU2", price = 20F, image = "image2", type = ProductType.HIGH_DEMAND)
 
         private val INVENTORY_1 = Inventory(id = 123, SKU = "SKU1", quantity = 100, zone = "zone1")
-        private val INVENTORY_2 = Inventory(id = 456, SKU = "SKU2", quantity = 50, zone = "zone2")
-        private val INVENTORY_3 = Inventory(id = 789, SKU = "SKU2", quantity = 70, zone = "zone3")
+        private val INVENTORY_2 = Inventory(id = 456, SKU = "SKU2", quantity = 90, zone = "zone2")
+        private val INVENTORY_3 = Inventory(id = 789, SKU = "SKU2", quantity = 20, zone = "zone3")
     }
 }
